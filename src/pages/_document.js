c5198dbd239e75a66635b598a4bf13ff6d1adda4
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-01-28 23:24:55 -0300

    next seo basic config

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,7 @@
 import Document from 'next/document'
+import Head from 'next/head'
+import { NextSeo } from 'next-seo'
+
 import { ServerStyleSheet } from 'styled-components'
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -18,6 +21,17 @@ export default class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            <Head>
+              <NextSeo 
+              title='Se7e | Estúdio de Design'
+              description='Estúdio de design focado em solução.'
+              canonical='http://www.visitase7e.com'
+              additionalLinkTags={[{
+                rel: 'icon',
+                href: '/favicon.ico'
+              }]}
+              />
+            </Head>
           </>
         ),
       }
