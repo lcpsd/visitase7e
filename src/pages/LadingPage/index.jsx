74b224c736890df86b3c334d35493e3b54e096a1
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-01-30 21:38:27 -0300

    module not found bug fix

--- src/pages/LadingPage/index.jsx
+++ src/pages/LadingPage/index.jsx
@@ -1,10 +1,10 @@
-import Header from '@/components/se7e/landingPage/Header'
-import HomeSection from '@/components/se7e/landingPage/HomeSection'
-import PlansSection from '@/components/se7e/landingPage/PlansSection'
-import ToolsSection from '@/components/se7e/landingPage/ToolsSection'
-import QAsection from '@/components/se7e/landingPage/QAsection'
-import ContactSection from '@/components/se7e/landingPage/ContactSection'
-import Goback from '@/components/se7e/landingPage/Goback'
+import Header from 'src/components/se7e/landingPage/Header'
+import HomeSection from 'src/components/se7e/landingPage/HomeSection'
+import PlansSection from 'src/components/se7e/landingPage/PlansSection'
+import ToolsSection from 'src/components/se7e/landingPage/ToolsSection'
+import QAsection from 'src/components/se7e/landingPage/QAsection'
+import ContactSection from 'src/components/se7e/landingPage/ContactSection'
+import Goback from 'src/components/se7e/landingPage/Goback'
 
 export default function LandingPage(){
 
