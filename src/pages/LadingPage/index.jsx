2b00616d376bcd1ae78a209cf7435b71592d5750
Lucas Cardoso <lucascardosopsd@gmail.com>
2022-02-01 11:01:25 -0300

    landingpage component names fixed

--- src/pages/LadingPage/index.jsx
+++ src/pages/LadingPage/index.jsx
@@ -1,11 +1,11 @@
 import Header from './components/Header'
 import  HomeSection  from './components/HomeSection'
-import  PlansSection  from './components/PlansSection/Index'
+import  PlansSection  from './components/PlansSection'
 import  ToolsSection  from './components/ToolsSection'
 import  QAsection  from './components/QAsection'
 import  ContactSection  from './components/ContactSection'
 import  Goback  from './components/GoBack'
-import Footer from './components/Footer/Index'
+import Footer from './components/Footer'
 
 export default function LandingPage(){
 
